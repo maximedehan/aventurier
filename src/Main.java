fc747dd56121ce58f86ac5216f3a9fa01736190c
maximedehan <maximedehan@aol.com>
2025-03-01 04:35:23 +0100

    traitement fichier entrée et transformation carte en matrice

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,47 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
 
 public class Main {
 	
   public static void main(String[] args) {
-    String path = "carte v2.txt";
+    String carteV2 = "carte v2.txt";
+	String guide = "guide.txt";
 	
     try {
-      FileReader reader = new FileReader(path);  
+	  // Lecture du fichier de guide et sauvegarde des données (coordonnées x, y, les directions)
+      FileReader readerGuide = new FileReader(guide); 
+	  BufferedReader bufferedReaderGuide = new BufferedReader(readerGuide);
+	  String line;
+	  ArrayList<String> linesGuide = new ArrayList<String>();
+	  while ((line = bufferedReaderGuide.readLine()) != null) {
+		line.replaceAll(" ", ""); // On enlève les espaces de la ligne
+		linesGuide.add(line);
+      }
+	  String[] coordXY = linesGuide.get(0).split(",");
+	  int coordX = Integer.parseInt(coordXY[0]), coordY = Integer.parseInt(coordXY[1]);
+	  String[] arrayDirections = linesGuide.get(1).split("");
+	  bufferedReaderGuide.close();
+      readerGuide.close();
 	  
-      while (reader.ready()) {    
-        System.out.print((char) reader.read());  
-      }    
+	  // On transforme la carte en matrice pour faciliter le traitement des déplacements sur la carte
+	  FileReader readerCarte = new FileReader(carteV2); 
+	  BufferedReader bufferedReaderCarte = new BufferedReader(readerCarte);
+	  String[][] matrixCarte = new String[20][20];
+	  int i = 0;
+	  while ((line = bufferedReaderCarte.readLine()) != null) {
+		for (int j=0; j<line.split("").length; j++) {
+			matrixCarte[i][j] = line.split("")[j];
+		}
+		i++;
+	  }
+	  bufferedReaderCarte.close();
+      readerCarte.close();
 	  
-      reader.close();
+	  // Déplacement sur la carte 
+	  
+
     } catch (IOException e) {
       System.out.println("There has been an IO exception!");
       System.out.println(e);
